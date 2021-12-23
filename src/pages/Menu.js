efa342d580535ae4f87d8d449a61379d8ec1a8e6
Michael Forero <mforeroroldan@gmail.com>
2021-12-23 13:01:12 -0500

    add menu in target

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import Mojarrita from '../pictures/Mojarrita.jpg'
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
+import menu from '../pictures/menu/menu'
 const Menu = () => {
+   const renderMenu = menu.map(({img}) =>(
+    <Card style={{ width: '18rem', display:'inline-flex', marginTop:"5px"}}>
+    <Card.Img variant="top" src={img} />
+    <Card.Body>
+      <Card.Title>Card Title</Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+      </Card.Text>
+    </Card.Body>
+    <ListGroup className="list-group-flush">
+      <ListGroupItem>Cras justo odio</ListGroupItem>
+      <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+      <ListGroupItem>Vestibulum at eros</ListGroupItem>
+    </ListGroup>
+    <Card.Body>
+      <Card.Link href="#">Card Link</Card.Link>
+      <Card.Link href="#">Another Link</Card.Link>
+    </Card.Body>
+  </Card>
+   ))
     return (
         <div>
             <h1>Menu</h1>
-            <img src={Mojarrita} />
+            {renderMenu}
         </div>
     )
 }
---
+++ src/pictures/menu/menu.js
@@ -0,0 +1,19 @@
+import CachamaSudada from './CachamaSudada.png'
+import Mojarra from './Mojarra.png'
+import MojarraAsada from './MojarraAsada.png'
+import Mojarrita from './Mojarrita.jpg'
+export default[
+    {
+        "img":Mojarra
+    },
+    {
+        "img":MojarraAsada
+    },
+    {
+        "img":Mojarrita
+    },
+    {
+        "img":CachamaSudada
+    }
+
+]
